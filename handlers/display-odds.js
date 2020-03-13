5a474aab5ca792c00708321989d02806f36e9885
KLisabeth <professional.translations4ppl@gmail.com>
2020-03-13 21:18:28 +0100

    display odds handler and listener

--- handlers/display-odds.js
+++ handlers/display-odds.js
@@ -1,7 +1,13 @@
 function oddsHandler() {
   debugger;
   // write me!
-
+ const listContainer = document.getElementById('selected');
+ listContainer.innerHTML = '';
+ for (let i = 0; i < strings.odds.length; i++) {
+   const currentListComponent= document.createElement('li');
+   currentListComponent.innerText = strings.odds[i];
+   listContainer.appendChild(currentListComponent);
+ }
   // log user interaction: handler name, user input, new state
   log.push({
     handler: 'odds',
