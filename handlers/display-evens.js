76c2e3850afeeb88a62268fec8e8c9bcafb1f667
KLisabeth <professional.translations4ppl@gmail.com>
2020-03-13 21:19:53 +0100

    display evens handler and listener

--- handlers/display-evens.js
+++ handlers/display-evens.js
@@ -1,7 +1,13 @@
 function evensHandler() {
   debugger;
   // write me!
-
+ const listContainer = document.getElementById('selected');
+ listContainer.innerHTML = '';
+ for (let i = 0; i < strings.evens.length; i++){
+   const currentListComponent =document.createElement('li');
+   currentListComponent.innerText = strings.evens[i];
+   listContainer.appendChild(currentListComponent);
+ }
   // log user interaction: handler name, user input, new state
   log.push({
     handler: 'evens',
