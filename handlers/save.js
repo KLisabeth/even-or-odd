13f2ae65e37c3ca10a322e62dab746d40a07c6af
KLisabeth <professional.translations4ppl@gmail.com>
2020-03-13 21:17:16 +0100

    save handler and listener

--- handlers/save.js
+++ handlers/save.js
@@ -1,8 +1,27 @@
 function saveHandler() {
   debugger;
   // write me!
+  const newInput = document.getElementById('input').value.trim();
 
-  // log user interaction: handler name, user input, new state
+  if (newInput === ''){ alert('Please type something');}
+  if (strings.NaNy.filter(function (i) { return i === newInput; }).length > 0) {
+    alert('This Nany character already exists!'); return false}
+  if (strings.evens.filter(function (i) { return i === newInput; }).length > 0){
+    alert('This evens character already exists!'); return false}
+  if (strings.odds.filter(function (i) { return i === newInput; }).length > 0){
+    alert('This odds character already exists!'); return false}
+  
+
+  if (isNaN(newInput)) {
+    strings.NaNy.push(newInput);
+  } else if (Number(newInput % 2 == 0)) {
+    strings.evens.push(newInput);
+  } else {
+    strings.odds.push(newInput);
+  }
+
+  //log user interaction: handler name, user input, new state
+
   log.push({
     handler: 'save',
     newInput,
